59f2b7baf0ec914f784045051228d607807e2bbc
Vibhakti Patil <At-Mykhop>
2023-04-04 15:51:52 +0530

    Second commit

---
+++ dayone/src/org/TNSIF/introduction/CharDemo.java
@@ -0,0 +1,17 @@
+package org.TNSIF.introduction;
+
+public class CharDemo {
+
+	public static void main(String[] args) {
+		char ch='a';
+		System.out.println(ch);
+		//assigning num to ASCI
+		char ch1=70;
+		System.out.println(ch1);
+		// assigning unicode to char
+		char ch2 ='\u00A3';
+		System.out.println(ch2);
+
+	}
+
+}
---
+++ dayone/src/org/TNSIF/introduction/DataTypesDemo.java
@@ -0,0 +1,22 @@
+//program to demonstrate on various Datatype
+package org.TNSIF.introduction;
+
+public class DataTypesDemo {
+
+	public static void main(String[] args) {
+		//integer will give us the values in whole number
+		//float and double will gives us the value 
+		int num1=5/2;
+		float num2=102/45;
+		/* if we use d after any number for double type it will give 
+		 * *the precision upto 16 decimal places
+		 */
+		double num3=10d/3d;
+		float num4=14f/3f;
+		System.out.print("the value1 is:"+num1);
+		System.out.print("the value2 is:"+num2);
+		System.out.print("the value3 is:"+num3);
+		System.out.print("the value4 is:"+num4);
+	}
+
+}
