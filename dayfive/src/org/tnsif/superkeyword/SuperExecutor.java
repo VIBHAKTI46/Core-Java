2e32bac56ea48207072d13c08a2c87a8b44136f0
Vibhakti Patil <At-Mykhop>
2023-04-14 23:13:36 +0530

    nineth comment

---
+++ dayfive/src/org/tnsif/superkeyword/SuperExecutor.java
@@ -0,0 +1,12 @@
+//Program to demonstrate on super keyword
+package org.tnsif.superkeyword;
+
+public class SuperExecutor {
+ public static void main(String[] args) {
+	 Tiger t=new Tiger();
+	 t.display();
+	 //it will access child class properties
+	 System.out.print(t);
+	 
+ }
+}
