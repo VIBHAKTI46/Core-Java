eee8ac04feb9941b073e97f9224d8962d49c4090
Vibhakti Patil <At-Mykhop>
2023-05-28 20:27:40 +0530

    first commit

--- dayfour/src/org/tnsif/secondpackage/Executor.java
+++ dayfour/src/org/tnsif/secondpackage/Executor.java
@@ -1,13 +1,17 @@
 package org.tnsif.secondpackage;
 //import org.tnsif.firstpackage.*;
 //way to import the another package
+//import- package name-class name
 import org.tnsif.firstpackage.Base;
 public class Executor {
 
 	public static void main(String[] args) {
 		Base b =new Base();
-		//if any method is deafult we cannot acess into another packge
+		//if any method is deafult we cannot acess into another package
+		//we can access only inside same package
 		b.methodpublic();
+
+		
 	}
 
 }
