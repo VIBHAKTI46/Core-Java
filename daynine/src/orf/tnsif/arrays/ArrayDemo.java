c0222f4448583499795f766d5dbfeec8ab953c9d
Vibhakti Patil <At-Mykhop>
2023-06-12 21:34:00 +0530

    Second Commit

--- daynine/src/orf/tnsif/arrays/ArrayDemo.java
+++ daynine/src/orf/tnsif/arrays/ArrayDemo.java
@@ -1,10 +1,21 @@
+//program on simple array
 package orf.tnsif.arrays;
 
 public class ArrayDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		//compile time array 
+		int arr [] = {12,6,19,67};
+		//if we access 4th index we will get exception 
+		//as arrayindexoutofboundexception
+		//System.out.println(arr[4]);
+		
+		System.out.println("Array of 2nd index :"+arr[2]);
+		//for printing all the element arrayusing enhanced for loop
+		for (int i :arr)
+		{
+			System.out.println(i+"");
+		}
 	}
 
 }
