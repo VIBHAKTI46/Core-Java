50e02b6f18e9ee23a2b1fbf5e5ab541671f80c78
Vibhakti Patil <At-Mykhop>
2023-06-16 09:17:49 +0530

    first commit

--- MoneyMoneyBank/src/org/tnsif/client/MoneyMoneyBankApplicationClient.java
+++ MoneyMoneyBank/src/org/tnsif/client/MoneyMoneyBankApplicationClient.java
@@ -11,7 +11,7 @@ public class MoneyMoneyBankApplicationClient{
 	public static void main(String[] args) {
 		BankFactory b = new MMBankFactory();
 		savingAcc s = new MMSavingAcc(101, "vibhakti A. Patil", 4000, true);
-		CurrentAcc c = new MMCurrentAcc(102, "Ankit A. Patil", 5000, 10000);
+		CurrentAcc c = new MMCurrentAcc(102, "Ankit A. Patil", 500, 10000);
 		System.out.println("Savings Account");
 		s.withdraw(s.getAccBal());
 		System.out.println("Current Account");
